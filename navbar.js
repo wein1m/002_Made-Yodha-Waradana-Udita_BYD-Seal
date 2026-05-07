5d0fcfe1efbdcd794d333a2fa89bc66fa18f0f6f
wein1m <weynn.sz@gmail.com>
2026-05-08 02:20:22 +0800

    feat(nav): implement open/close mobile nav

---
+++ navbar.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".hamburg-btn");
+  const wrapper = document.querySelector(".navs-mobile");
+
+  const logo = document.querySelector("nav .logo");
+  const hamburgWrapper = document.querySelector(".navs-mobile-hamburg");
+  const cta = document.querySelector(".navs-mobile-hamburg a");
+
+  const openSVG = `
+    <svg viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(0)matrix(1, 0, 0, 1, 0, 0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M4 7C4 6.44771 4.44772 6 5 6H24C24.5523 6 25 6.44771 25 7C25 7.55229 24.5523 8 24 8H5C4.44772 8 4 7.55229 4 7Z" fill="#000000"></path><path d="M4 13.9998C4 13.4475 4.44772 12.9997 5 12.9997L16 13C16.5523 13 17 13.4477 17 14C17 14.5523 16.5523 15 16 15L5 14.9998C4.44772 14.9998 4 14.552 4 13.9998Z" fill="#000000"></path><path d="M5 19.9998C4.44772 19.9998 4 20.4475 4 20.9998C4 21.552 4.44772 21.9997 5 21.9997H22C22.5523 21.9997 23 21.552 23 20.9998C23 20.4475 22.5523 19.9998 22 19.9998H5Z" fill="#000000"></path></g></svg>
+  `;
+  const closeSVG = `
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z" fill="#0F1729"></path> </g></svg>
+   `;
+
+  btn.addEventListener("click", () => {
+    wrapper.classList.toggle("open");
+
+    if (wrapper.classList.contains("open")) {
+      logo.style.backgroundColor = "transparent";
+      hamburgWrapper.style.backgroundColor = "transparent";
+      cta.style.display = "none";
+
+      btn.innerHTML = closeSVG;
+      document.body.style.overflow = "hidden";
+    }
+    else {
+      logo.style.backgroundColor = "";
+      hamburgWrapper.style.backgroundColor = "";
+      cta.style.display = "";
+
+      btn.innerHTML = openSVG;
+      document.body.style.overflow = "";
+    }
+  });
+});
