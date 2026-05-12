7662fd5b9166b6e0bfcbeaa6422fc43728c6e0fa
wein1m <weynn.sz@gmail.com>
2026-05-13 00:03:52 +0800

    refactor: combine navHover to navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+  hamburgerHandler();
+  navItemHover();
+});
+
+function hamburgerHandler() {
   const btn = document.querySelector(".hamburg-btn");
   const wrapper = document.querySelector(".navs-mobile");
 
@@ -40,4 +45,31 @@ document.addEventListener("DOMContentLoaded", () => {
       closeMenu();
     }
   });
-});
+}
+
+function navItemHover() {
+  const navWrapper = document.querySelector(".navs-mobile ul");
+  const navItems = document.querySelectorAll(".navs-mobile ul li");
+
+  let lastHovered = 0;
+
+  navItems.forEach((e, i) => {
+    e.addEventListener("mouseenter", () => {
+      e.style.cursor = "pointer";
+      e.classList.add("hovered");
+    });
+    e.addEventListener("mouseleave", () => {
+      lastHovered = i;
+      e.style.cursor = "default";
+      e.classList.remove("hovered");
+    });
+  });
+
+  navWrapper.addEventListener("mouseenter", () => {
+    navItems.forEach((e) => e.classList.remove("hovered"));
+  });
+
+  navWrapper.addEventListener("mouseleave", () => {
+    navItems[lastHovered].classList.add("hovered");
+  });
+}
